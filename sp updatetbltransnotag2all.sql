09f76e46f7276868f49e90f614679026437b512c
hendrawijaya83 <cm.hendraw@gmail.com>
2025-12-11 07:21:56 +0700

    Update sp updatetbltransnotag2all.sql

--- sp updatetbltransnotag2all.sql
+++ sp updatetbltransnotag2all.sql
@@ -91,6 +91,11 @@ END WHILE;
 select COUNT(notrans) into n from tbltagjb where (tipejb='jualb' or tipejb='jualp' or tipejb='jualc') and berat<>0;
 SET i=0;
 WHILE i<n DO 
+        SELECT xp.notag,xp.tgljb,xp.addby,xp.berat,xp.notrans,xp.itemid,xp.adddate,x.operator,x.qc,xp.kodemesin,xp.noplan,xp.shiftid,y.notag,y.notrans,y.tgllap,y.noplan 
+        into strnotag,dateTag,strUser,decQty2,strnotrans,litemid2,dateadddate,strop,strqc,strmesin,lnoplan,lshiftid,strnotag2,strnotrans2,datetag2,lnoplan2
+        FROM  tbltagjb xp join tbllaptag y on xp.notagblowing=y.notag 
+        where xp.tgljb>='2025-08-31' and y.tgllap>='2025-08-31'
+        and xp.berat<>0 and y.berat<>0 LIMIT i,1;
 
         SELECT notag,tgljb,editby,berat,notrans,itemid,editdate,'','',kodemesin,0,1,tipejb
         into strnotag,dateTag2,strUser,decQty2,strnotrans,litemid2,dateadddate,strop,strqc,strmesin
@@ -126,6 +131,7 @@ WHILE i<n DO
 END WHILE;
 
 End
+
 
 
 
