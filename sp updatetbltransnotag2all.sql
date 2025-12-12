83187ede97dd812780379333a51704db62a71cbf
hendrawijaya83 <cm.hendraw@gmail.com>
2025-12-12 15:34:03 +0700

    Update sp updatetbltransnotag2all.sql

--- sp updatetbltransnotag2all.sql
+++ sp updatetbltransnotag2all.sql
@@ -75,7 +75,8 @@ select count(xp.notag) into n from tbllaptagp x right join tbltagp xp on x.notag
 
 SET i=0;
 WHILE i<n DO 
-        SELECT xp.notag,xp.adddate2,xp.addby,xp.berat,xp.notrans,xp.itemid,xp.adddate,x.operator,x.qc,xp.kodemesin,xp.noplan,xp.shiftid,y.notag,y.notrans,y.tgllap,y.noplan 
+        SELECT xp.notag,xp.adddate2,xp.addby,xp.berat,xp.notrans,xp.itemid,xp.adddate,x.operator,x.qc,xp.kodemesin,xp.noplan,
+        xp.shiftid,y.notag,y.notrans,y.tgllap,y.noplan 
         into strnotag,dateTag,strUser,decQty2,strnotrans,litemid2,dateadddate,strop,strqc,strmesin,lnoplan,lshiftid,strnotag2,strnotrans2,datetag2,lnoplan2
         FROM tbllaptagp x right join tbltagp xp on x.notag=xp.notag left join tbllaptag y on xp.notagblowing=y.notag 
         where xp.adddate2>='2025-08-31' and y.tgllap>='2025-08-31'
@@ -88,49 +89,66 @@ WHILE i<n DO
     SET i = i + 1;
 END WHILE;
 
-select COUNT(notrans) into n from tbltagjb where (tipejb='jualb' or tipejb='jualp' or tipejb='jualc') and berat<>0;
-SET i=0;
-WHILE i<n DO 
-        SELECT xp.notag,xp.tgljb,xp.addby,xp.berat,xp.notrans,xp.itemid,xp.adddate,x.operator,x.qc,xp.kodemesin,xp.noplan,xp.shiftid,y.notag,y.notrans,y.tgllap,y.noplan 
-        into strnotag,dateTag,strUser,decQty2,strnotrans,litemid2,dateadddate,strop,strqc,strmesin,lnoplan,lshiftid,strnotag2,strnotrans2,datetag2,lnoplan2
-        FROM  tbltagjb xp join tbllaptag y on xp.notagblowing=y.notag 
-        where xp.tgljb>='2025-08-31' and y.tgllap>='2025-08-31'
-        and xp.berat<>0 and y.berat<>0 LIMIT i,1;
-
-        SELECT notag,tgljb,editby,berat,notrans,itemid,editdate,'','',kodemesin,0,1,tipejb
-        into strnotag,dateTag2,strUser,decQty2,strnotrans,litemid2,dateadddate,strop,strqc,strmesin
-        ,lnoplan,lshiftid,strtipe
-        FROM tbltagjb 
-        where (tipejb='jualb' or tipejb='jualp' or tipejb='jualc') and berat<>0 LIMIT i,1;
-
-        if strnotag<>'' then
-                call updatetbltransnotag2 (dateTag2,strnotag,strnotrans,strtipe,'','',dateTag2,
-            dateadddate,struser,'add',strop,strqc,strmesin,litemid2,decQty2,lnoplan,lshiftid,lnoplan); 
-        end if;
-    SET i = i + 1;
-END WHILE;
-
-/*sampai sini dulu y*/
-SELECT COUNT(c.notagblowing) into n from tbltagc c where c.berat<>0 and c.notagblowing<>'' and c.adddate2>='2025-08-31';
+SELECT COUNT(c.notagblowing) into n from tbltagc c join tbllaptag b on b.notag=c.notagblowing and c.notagblowing<>'' 
+        where c.berat<>0 and b.berat<>0 and c.adddate2>='2025-08-31' and b.tgllap>='2025-08-31' ;
 SET i=0;
 
 WHILE i<n DO 
 
-        SELECT notag,adddate2,editby,berat,notrans,itemid,editdate,'','',kodemesin,0,1,'cutting',notagblowing
+        SELECT c.notag,c.adddate2,c.editby,b.berat,c.notrans,b.itemid,c.editdate,'','','',0,1,'cutting',b.notag,b.notrans,b.tgllap
         into strnotag,dateTag2,strUser,decQty2,strnotrans,litemid2,dateadddate,strop,strqc,strmesin,
-        ,lnoplan,lshiftid,strtipe
-        FROM tbltagc
-        where notagblowing<>'' and berat<>0 and adddate2>='2025-08-31' LIMIT i,1;
+        ,lnoplan,lshiftid,strtipe,strnotag2,strnotrans2,datetag2
+        FROM tbltagc c join tbllaptag b on b.notag=c.notagblowing and c.notagblowing<>'' 
+        where c.berat<>0 and b.berat<>0 and c.adddate2>='2025-08-31' and b.tgllap>='2025-08-31' limit i,1 ;
 
         if strnotrans<>'' then
-                call updatetbltransnotag2 (dateTag2,strnotag,strnotrans,strtipe,'','',dateTag2,
+                call updatetbltransnotag2 (dateTag,strnotag,strnotrans,strtipe,strnotag2,strnotrans2,dateTag2,
             dateadddate,struser,'add',strop,strqc,strmesin,litemid2,decQty2,lnoplan,lshiftid,lnoplan); 
         end if;
     SET i = i + 1;
 
+END WHILE;
+
+SELECT COUNT(c.notagprinting) into n from tbltagc c join tbllaptagp b on b.notag=c.notagprinting and c.notagprinting<>'' 
+        where c.berat<>0 and b.berat<>0 and c.adddate2>='2025-08-31' and b.tgllap>='2025-08-31' ;
+SET i=0;
+
+WHILE i<n DO 
+
+        SELECT c.notag,c.adddate2,c.editby,b.berat,c.notrans,b.itemid,c.editdate,'','','',1,1,'cutting',b.notag,b.notrans,b.tgllap
+        into strnotag,dateTag2,strUser,decQty2,strnotrans,litemid2,dateadddate,strop,strqc,strmesin,
+        ,lnoplan,lshiftid,strtipe,strnotag2,strnotrans2,datetag2
+        FROM tbltagc c join tbllaptagp b on b.notag=c.notagprinting and c.notagprinting<>'' 
+        where c.berat<>0 and b.berat<>0 and c.adddate2>='2025-08-31' and b.tgllap>='2025-08-31' limit i,1 ;
+
+        if strnotrans<>'' then
+                call updatetbltransnotag2 (dateTag,strnotag,strnotrans,strtipe,strnotag2,strnotrans2,dateTag2,
+            dateadddate,struser,'add',strop,strqc,strmesin,litemid2,decQty2,lnoplan,lshiftid,lnoplan); 
+        end if;
+    SET i = i + 1;
+
+END WHILE;
+
+select COUNT(jb.notrans) into n from tbltagjb jb join tbltransnotag2 y on jb.notag=y.notag and left(y.tipetrans,1)=right(jb.tipejb,1)  
+        where (jb.tipejb='jualb' or jb.tipejb='jualp' or jb.tipejb='jualc') and y.berat<>0 and y.notag<>'' ;
+SET i=0;
+WHILE i<n DO 
+        SELECT jb.notag,jb.tgljb,jb.editby,y.berat,jb.notrans,y.itemid,jb.editdate,'','','',0,1,y.notag,
+        y.notrans,y.tgltrans,0,jb.tipejb 
+        into strnotag,dateTag,strUser,decQty2,strnotrans,litemid2,dateadddate,strop,strqc,strmesin,lnoplan,lshiftid,strnotag2,
+        strnotrans2,datetag2,lnoplan2,strtipe
+        FROM  tbltagjb jb join tbltransnotag2 y on jb.notag=y.notag and left(y.tipetrans,1)=right(jb.tipejb,1) 
+        where (jb.tipejb='jualb' or jb.tipejb='jualp' or jb.tipejb='jualc') and y.berat<>0 and y.notag<>'' limit i,1;
+
+        if strnotag<>'' then
+                call updatetbltransnotag2 (dateTag,strnotag,strnotrans,strtipe,strnotag2,strnotrans2,dateTag2,
+            dateadddate,struser,'add',strop,strqc,strmesin,litemid2,decQty2,lnoplan,lshiftid,lnoplan); 
+        end if;
+    SET i = i + 1;
 END WHILE;
 
 End
+
 
 
 
